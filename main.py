a0e56ba2e86522f0ba66c5abfb0892539be981db
dembogurski <dembogurski@gmail.com>
2023-04-13 18:31:00 -0400

    Primeras pruebas primer commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hola mundo!!!")
+
+a = 4
+b = 2
+c = a + (b + b) * a
+
+print('a = {} '.format(a))
+print('b = {} '.format(b))
+print('c = {} '.format(c))
+
+arr = ['Hola', 'mundo', 'de', 'python']
+
+print(arr)
+
+#print(arr[1])
+
+arr[1] = "programador"
+
+print(arr)
+
+#print(arr[1])
+
+for e in arr:
+    print(e)
+
+
+# arrb = ''.join(arr)
